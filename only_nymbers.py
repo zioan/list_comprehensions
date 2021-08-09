de296d8fbf91eb46501dde13e7881496466a5438
zioan <zaharia.ioan@gmail.com>
2021-08-09 12:11:59 +0200

    more

--- only_nymbers.py
+++ only_nymbers.py
@@ -4,4 +4,23 @@ temps = [221, 234, 340, -9999, 230]
 new_temps = [temp / 10 for temp in temps if temp != -9999]
 print(new_temps)
   #[22.1, 23.4, 34.0, 23.0]
-  #-9999 is excluded from the 
+  #-9999 is excluded from the loop/execution
+
+#will display only numbers, no strings
+def foo(lst):
+    new_lst = []
+    for i in lst:
+        if isinstance(i, int):
+            new_lst.append(i)
+    return new_lst
+
+  
+print(foo([99, "no data", 45]))
+
+#will display only positive numbers
+numbers = [-5, 3, -1, 101]
+ 
+def num(numbers):
+    return [numb for numb in numbers if numb > 0]
+ 
+print(num(numbers))
